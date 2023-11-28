5e9a882258765894292a6107a6a73a60fbd9e069
JorgeHDZCA <123972008+JorgeHDZCA@users.noreply.github.com>
2023-11-27 21:46:17 -0600

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Btn_Entrar = New System.Windows.Forms.Button()
+        Me.TxtClave = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Btn_Entrar
+        '
+        Me.Btn_Entrar.Location = New System.Drawing.Point(391, 273)
+        Me.Btn_Entrar.Name = "Btn_Entrar"
+        Me.Btn_Entrar.Size = New System.Drawing.Size(170, 79)
+        Me.Btn_Entrar.TabIndex = 0
+        Me.Btn_Entrar.Text = "Entrar"
+        Me.Btn_Entrar.UseVisualStyleBackColor = True
+        '
+        'TxtClave
+        '
+        Me.TxtClave.Location = New System.Drawing.Point(244, 181)
+        Me.TxtClave.Name = "TxtClave"
+        Me.TxtClave.Size = New System.Drawing.Size(304, 22)
+        Me.TxtClave.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TxtClave)
+        Me.Controls.Add(Me.Btn_Entrar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Btn_Entrar As Button
+    Friend WithEvents TxtClave As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Data.SqlClient
+Imports System.Runtime.CompilerServices
+
+Public Class Form1
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+
+    End Sub
+
+    Private Sub Btn_Entrar_Click(sender As Object, e As EventArgs) Handles Btn_Entrar.Click
+        If (TxtClave.Text = "") Then
+            MessageBox.Show("ingrese su clave")
+            TxtClave.Focus()
+        Else
+            Using connection As New SqlConnection(connectionString)
+                connection.Open()
+                Dim fecha = DtpF.Value.ToString("yyyy/mm/dd")
+                Variables.Query = "insert into Asistencia(Fecha,Estado,id_cliente)'" + fecha + "',1" + TxtClave.Text + ""
+                Using comando As New SqlCommand(Query, connection)
+                    comando.Parameters.AddWithValue("fecha", fecha)
+                    comando.Parameters.AddWithValue("id_cliente", TxtClave.Text)
+                    Dim filasAfectadas As Integer = comando.ExecuteNonQuery()
+                    If filasAfectadas > 0 Then
+                        MessageBox.Show("Asistencia Registrada")
+                    Else
+                        MessageBox.Show("No se pudo insertar la asistencia")
+                    End If
+                End Using
+            End Using
+        End If
+    End Sub
+End Class
